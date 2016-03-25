c51ff61a73c10a63198886902c38adcf310e8228
brenopolanski <breno.polanski@gmail.com>
2016-03-25 19:13:21 -0300

    Add component <Icon /> add images of Saiku

---
+++ src/main/resources/src/js/components/saiku/Icon/Icon.jsx
@@ -0,0 +1,96 @@
+/**
+ *   Copyright 2016 OSBI Ltd
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+import React from 'react';
+import classNames from 'classnames';
+
+/**
+ * Saiku <Icon /> component.
+ * This component should display a icon of Font-Awesome.
+ *
+ * @example
+ * <Icon name="home" />
+ */
+class Icon extends React.Component {
+
+  /**
+   * React components implement the `render()` method that takes input data and
+   * returns what to display. This method uses an XML-like syntax called JSX.
+   * Input data that is passed into the component can be accessed by `render()`
+   * via this.props.
+   *
+   * @return {HTMLElement|Node|String} An image the company.
+   */
+  render() {
+    let {
+      name, fixed, spin, pulse,
+      size, rotate, flip
+    } = this.props;
+    let className = classNames(this.props.className, {
+      [this.props.faClass]: true,
+      [`fa-${name}`]: true,
+      ['fa-fw']: fixed,
+      ['fa-spin']: spin,
+      ['fa-pulse']: pulse
+    });
+
+    if (size) {
+      className = `${className} fa-${size}`;
+    }
+
+    if (rotate) {
+      className = `${className} fa-rotate-${rotate}`;
+    }
+
+    if (flip) {
+      className = `${className} fa-flip-${flip}`;
+    }
+
+    return (
+      <i
+        className={className}
+        {...this.props}
+        style={this.props.style}
+      >
+      </i>
+    );
+  }
+}
+
+Icon.displayName = 'Icon';
+
+Icon.propTypes = {
+  className: React.PropTypes.string,
+  style: React.PropTypes.string,
+  faClass: React.PropTypes.string,
+  name: React.PropTypes.string.isRequired,
+  fixed: React.PropTypes.bool,
+  spin: React.PropTypes.bool,
+  pulse: React.PropTypes.bool,
+  size: React.PropTypes.oneOf(['lg', '2x', '3x', '4x', '5x']),
+  rotate: React.PropTypes.oneOf(['45', '90', '135', '180',
+    '225', '270', '315']),
+  flip: React.PropTypes.oneOf(['horizontal', 'vertical'])
+};
+
+Icon.defaultProps = {
+  faClass: 'fa',
+  fixed: false,
+  spin: false,
+  pulse: false
+};
+
+export default Icon;
---
+++ src/main/resources/src/js/components/saiku/Icon/README.md
@@ -0,0 +1,59 @@
+# Saiku `<Icon />` component
+
+This component should display a icon of [Font Awesome](https://fortawesome.github.io/Font-Awesome/icons/).
+
+## Usage
+
+```javascript
+import { Icon } from '../react-saiku';
+```
+
+## Options
+
+### Props
+
+Attribute   | Options                                       | Default | Description
+---         | ---                                           | ---     | ---
+`className` | *string*                                      |         | The `className` props specifies one or more classnames for an element.
+`style`     | *string*                                      |         | The `style` props specifies an inline style for an element.
+`faClass`   | *string*                                      | `fa`    | The `faClass` props specifies the default class name `fa`.
+`name`      | *string*                                      |         | Name of the icon to use.
+`fixed`     | *boolean*                                     | `false` | Make buttons fixed width.
+`spin`      | *boolean*                                     | `false` | Spin the icon.
+`pulse`     | *boolean*                                     | `false` | Rotate icon with 8 steps (rather than smoothly).
+`size`      | `lg`, `2x`, `3x`, `4x`, `5x`                  |         | The icon scaling size.
+`rotate`    | `45`, `90`, `135`, `180`, `225`, `270`, `315` |         | The degree to rotate the icon.
+`flip`      | `horizontal`, `vertical`                      |         | Flip the icon's orientation.
+
+### Methods
+
+*None*
+
+### Events
+
+*None*
+
+## Example
+
+```javascript
+import React from 'react';
+import { Icon } from '../react-saiku';
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <Icon
+          className="super-crazy-colors"
+          name="home"
+          size="2x"
+          spin
+          style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+        />
+      </div>
+    );
+  }
+}
+```
+
+**[⬅ back](../)**&nbsp;&nbsp;&nbsp;&nbsp;**[⬆ back to top](#saiku-icon--component)**
